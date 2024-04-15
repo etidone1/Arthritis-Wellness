eb9b1a07bdd3fd4e7de77379377779a0bb3409ff
etidone1 <67707913+etidone1@users.noreply.github.com>
2024-04-15 00:18:36 -1200

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,14 @@ window.addEventListener("scroll",() => {
         bang.classList.remove("active");
     }
 });
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwmyE9EyCKRAXoonmIsqfNfVMeS5qvbwL79_wwH0cF0yajwPt1we7zIv4fu_nOZAMwuWw/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  });
+
